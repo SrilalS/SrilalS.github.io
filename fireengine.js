b4c903382038d0d87705f6ce0aebf0050b3f6d7b
Srilal Sachintha <37685396+SrilalS@users.noreply.github.com>
2020-04-04 19:01:34 +0530

    ෆික්ස්

---
+++ fireengine.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCjr5jxvyRGRKUqp5cNg4-OnOVRTSSnyw8",
+    authDomain: "back-end-engine.firebaseapp.com",
+    databaseURL: "https://back-end-engine.firebaseio.com",
+    projectId: "back-end-engine",
+    storageBucket: "back-end-engine.appspot.com",
+    messagingSenderId: "337513006723",
+    appId: "1:337513006723:web:0cd1cabed0ae48ae78bdb6",
+    measurementId: "G-XK9FP3CX04"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
