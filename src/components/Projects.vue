63f9b0df7debf7faf624550e600d5a408de8a1fe
Srilal Sachintha <SrilalSachintha@Gmail.com>
2021-02-17 23:36:24 +0530

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -7,8 +7,8 @@
           <v-card-title>{{project.Name}}</v-card-title>
           <v-card-text>{{project.Description}}</v-card-text>
           <v-card-actions>
-            <v-btn dark color="#FF2B2B" :href="project.GitHub">GitHub</v-btn>
-            <v-btn v-if="project.LINKS.LINK !==''" dark color="#FF2B2B" :href="project.LINKS.LINK">{{ project.LINKS.Text }}</v-btn>
+            <v-btn target="_blank" dark color="#FF2B2B" :href="project.GitHub">GitHub</v-btn>
+            <v-btn target="_blank" v-if="project.LINKS.LINK !==''" dark color="#FF2B2B" :href="project.LINKS.LINK">{{ project.LINKS.Text }}</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -25,9 +25,9 @@ export default {
         {
           'Name':'U Music',
           'Description':'An OpenSource Music Player Build in Flutter. With Over 10,000 Download in Google Play',
-          'GitHub':'LINKHERE',
+          'GitHub':'https://github.com/SrilalS/U-Music',
           'LINKS':{
-            'LINK':'x',
+            'LINK':'https://play.google.com/store/apps/details?id=com.srilals.umusicv2',
             'Text':'Google Play'
           }
         },
@@ -43,25 +43,34 @@ export default {
         {
           'Name':'Payhere Flutter Plugin',
           'Description':'An Community Payhere Plugin for Flutter. Easier Usage + Simple Syntax',
-          'GitHub':'LINKHERE',
+          'GitHub':'https://github.com/SrilalS/Payhere-Flutter-Plugin',
           'LINKS':{
-            'LINK':'XX',
+            'LINK':'https://pub.dev/packages/payhere',
             'Text':'Pub.dev'
           }
         },
         {
           'Name':'QR-Toolkit',
           'Description':'Lightning Fast QR Reader and Creator Made Using Flutter',
-          'GitHub':'LINKHERE',
+          'GitHub':'https://github.com/SrilalS/QR-Toolkit',
           'LINKS':{
-            'LINK':'XX',
+            'LINK':'https://play.google.com/store/apps/details?id=com.srilal.qrtoolkit',
             'Text':'Google Play'
+          }
+        },
+        {
+          'Name':'Space Dashboard',
+          'Description':'Usage of NASA Public APIs. Displays the Image of the Day and NEOs of the Week',
+          'GitHub':'https://github.com/SrilalS/SpaceDashboard',
+          'LINKS':{
+            'LINK':'https://srilalsachintha.me/SpaceDashboard/',
+            'Text':'View'
           }
         },
         {
           'Name':'Advanced Ad Blocker',
           'Description':'Simplified Host File Based Ad Blocking',
-          'GitHub':'LINKHERE',
+          'GitHub':'https://github.com/SrilalS/Advanced-Ad-Blocker',
           'LINKS':{
             'LINK':'',
             'Text':''
@@ -70,7 +79,7 @@ export default {
         {
           'Name':'Foodie',
           'Description':'A University REST API Project',
-          'GitHub':'LINKHERE',
+          'GitHub':'https://github.com/SrilalS/Foodie',
           'LINKS':{
             'LINK':'',
             'Text':''
