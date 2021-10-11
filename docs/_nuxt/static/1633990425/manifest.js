237abcde80bbbad4874b67177f606a532362751e
Srilal Sachintha <SrilalSachintha@Gmail.com>
2021-10-12 03:44:12 +0530

    nuxt proper build

---
+++ docs/_nuxt/static/1633990425/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Finspire","\u002Farticles","\u002Fprojects","\u002Fexperience","\u002Fcertificates","\u002Ftechnologies"]})
--- nuxt.config.js
+++ nuxt.config.js
@@ -65,5 +65,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  generate: {
+    dir:'docs'
   }
 }
